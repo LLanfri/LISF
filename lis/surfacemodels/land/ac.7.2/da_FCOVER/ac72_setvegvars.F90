ecc5d8a4e71c083d02fc8b4a1da070b86fd1eb36
vsc37433 <vsc37433@l33c32n3.tier2.hpc.kuleuven.be>
2025-06-04 13:30:28 +0200

    ENH: Added intermittent CC perturb application

--- lis/surfacemodels/land/ac.7.2/da_FCOVER/ac72_setvegvars.F90
+++ lis/surfacemodels/land/ac.7.2/da_FCOVER/ac72_setvegvars.F90
@@ -46,7 +46,8 @@ subroutine ac72_setvegvars(n, LSM_State)
   call LIS_verify(status)
 
   do t=1,LIS_rc%npatch(n,LIS_rc%lsm_index)
-    if (AC72_struc(n)%ac72(t)%Simulation%ProtectedSeedling == .false.) then
+    if ((AC72_struc(n)%ac72(t)%Simulation%ProtectedSeedling == .false.) .and.&
+      (mod(real(AC72_struc(n)%AC72(t)%daynri),5)==0)) then
       if (AC72CCiprev(t) > AC72_struc(n)%ac72(t)%CCiPot) then
         AC72CCiprev(t) = AC72_struc(n)%ac72(t)%CCiPot
       endif
