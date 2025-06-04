be74bb2cc740dd146ee77481103b6bf10c4c82ce
vsc37433 <vsc37433@l33c32n3.tier2.hpc.kuleuven.be>
2025-06-04 15:35:12 +0200

    BUG: Removal of the last ENH commit

--- lis/surfacemodels/land/ac.7.2/da_FCOVER/ac72_setvegvars.F90
+++ lis/surfacemodels/land/ac.7.2/da_FCOVER/ac72_setvegvars.F90
@@ -46,17 +46,14 @@ subroutine ac72_setvegvars(n, LSM_State)
   call LIS_verify(status)
 
   do t=1,LIS_rc%npatch(n,LIS_rc%lsm_index)
-    if ((AC72_struc(n)%ac72(t)%Simulation%ProtectedSeedling == .false.) .and.&
-      (mod(AC72_struc(n)%AC72(t)%daynri,5)==0)) then
-      if (AC72CCiprev(t) > AC72_struc(n)%ac72(t)%CCiPot) then
-        AC72CCiprev(t) = AC72_struc(n)%ac72(t)%CCiPot
-      endif
-      AC72_struc(n)%ac72(t)%CCiprev = AC72CCiprev(t)
-      AC72_struc(n)%ac72(t)%CCiActual = AC72CCiprev(t)
-      AC72_struc(n)%ac72(t)%crop%CCxAdjusted = AC72_struc(n)%ac72(t)%CCiActual
-      if (AC72_struc(n)%ac72(t)%CCiActual > AC72_struc(n)%ac72(t)%CCiTopEarlySen) then
-        AC72_struc(n)%ac72(t)%CCiTopEarlySen = AC72_struc(n)%ac72(t)%CCiActual
-      endif
+    if (AC72CCiprev(t) > AC72_struc(n)%ac72(t)%CCiPot) then
+      AC72CCiprev(t) = AC72_struc(n)%ac72(t)%CCiPot
+    endif
+    AC72_struc(n)%ac72(t)%CCiprev = AC72CCiprev(t)
+    AC72_struc(n)%ac72(t)%CCiActual = AC72CCiprev(t)
+    AC72_struc(n)%ac72(t)%crop%CCxAdjusted = AC72_struc(n)%ac72(t)%CCiActual
+    if (AC72_struc(n)%ac72(t)%CCiActual > AC72_struc(n)%ac72(t)%CCiTopEarlySen) then
+      AC72_struc(n)%ac72(t)%CCiTopEarlySen = AC72_struc(n)%ac72(t)%CCiActual
     endif
   enddo
   
